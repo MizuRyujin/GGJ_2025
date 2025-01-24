cfced92688af2f919e8a51991bc1cd8072a1d091
Miguel Fernández <miguelrf95@gmail.com>
2025-01-24 22:41:14 +0000

    Delete unused cubemap skyboxes. Add Magic Bubble font and create font asset. Initial Game scene with intro camera and menu

--- Assets/Scripts/BouncyPlatform.cs
+++ Assets/Scripts/BouncyPlatform.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class BouncyPlatform : MonoBehaviour
 {
     [SerializeField] private float _bounceStrenght;
+    private Vector3 _position;
 
     private void OnCollisionEnter(Collision other)
     {
         other.collider.attachedRigidbody.AddForce(0, _bounceStrenght, 0);
+    }
+
+    private IEnumerator Bounce(float timer, int maxDuration)
+    {
+        while (timer < maxDuration)
+        {
+
+            yield return null;
+        }
     }
 }
